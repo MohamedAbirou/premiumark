7ed23aa50ba19439adc16830365749e12e632345
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-12-05 00:46:31 +0100

    fix: @tailwindcss/line-clamp not found

--- src/app/lib/validators/query-validator.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-export const QueryValidator = z.object({
-  category: z.string().optional(),
-  sort: z.enum(['asc', 'desc']).optional(),
-  limit: z.number().optional(),
-})
-
-export type TQueryValidator = z.infer<typeof QueryValidator>
