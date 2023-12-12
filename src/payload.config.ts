69efb9c76905c1e139efec6e40230c20e5a76fdf
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-12-12 16:29:18 +0100

    chore: updated payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,32 +1,32 @@
-import { buildConfig } from 'payload/config'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { slateEditor } from '@payloadcms/richtext-slate'
-import path from 'path'
-import { Users } from './collections/Users'
-import dotenv from 'dotenv'
-import { Products } from './collections/Products/Products'
-import { Media } from './collections/Media'
-import { ProductFiles } from './collections/ProductFile'
-import { Orders } from './collections/Orders'
+import { buildConfig } from "payload/config";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import path from "path";
+import { Users } from "./collections/Users";
+import dotenv from "dotenv";
+import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFile";
+import { Orders } from "./collections/Orders";
 
 dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
-})
+  path: path.resolve(__dirname, "../.env"),
+});
 
 export default buildConfig({
-  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users, Products, Media, ProductFiles, Orders], 
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
-    admin: '/sell',
+    admin: "/sell",
   },
   admin: {
-    user: 'users',
+    user: "users",
     bundler: webpackBundler(),
     meta: {
-      titleSuffix: '- Premiumark',
-      favicon: '/favicon.ico',
-      ogImage: '/thumbnail.jpg',
+      titleSuffix: "- Premiumark",
+      favicon: "/favicon.ico",
+      ogImage: "/favicon.ico",
     },
   },
   rateLimit: {
@@ -37,6 +37,6 @@ export default buildConfig({
     url: process.env.MONGODB_URL!,
   }),
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
-})
+});
