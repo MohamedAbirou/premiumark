c09b381819b154a33c1257ba7a2d74d2315edb75
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-12-12 14:10:38 +0100

    fix: nodemailer sendgrid

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,65 +1,63 @@
-import dotenv from 'dotenv'
-import path from 'path'
-import type { InitOptions } from 'payload/config'
-import payload, { Payload } from 'payload'
-import nodemailer from 'nodemailer'
-import nodemailerSendgrid from "nodemailer-sendgrid-transport"
+import dotenv from "dotenv";
+import path from "path";
+import type { InitOptions } from "payload/config";
+import payload, { Payload } from "payload";
+import nodemailer from "nodemailer";
+import nodemailerSendgrid from "nodemailer-sendgrid";
 
 dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
-})
+  path: path.resolve(__dirname, "../.env"),
+});
 
 const transporter = nodemailer.createTransport(
   nodemailerSendgrid({
-    auth: {
-      api_key: process.env.SENDGRID_API_KEY,
-    },
+    apiKey: process.env.SENDGRID_API_KEY || "",
   })
-)
+);
 
-let cached = (global as any).payload
+let cached = (global as any).payload;
 
 if (!cached) {
   cached = (global as any).payload = {
     client: null,
     promise: null,
-  }
+  };
 }
 
 interface Args {
-  initOptions?: Partial<InitOptions>
+  initOptions?: Partial<InitOptions>;
 }
 
 export const getPayloadClient = async ({
   initOptions,
 }: Args = {}): Promise<Payload> => {
   if (!process.env.PAYLOAD_SECRET) {
-    throw new Error('PAYLOAD_SECRET is missing')
+    throw new Error("PAYLOAD_SECRET is missing");
   }
 
   if (cached.client) {
-    return cached.client
+    return cached.client;
   }
 
   if (!cached.promise) {
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: 'premiumark.business@gmail.com',
-        fromName: 'Premiumark',
+        fromAddress: "premiumark.business@gmail.com",
+        fromName: "Premiumark",
       },
       secret: process.env.PAYLOAD_SECRET,
       local: initOptions?.express ? false : true,
       ...(initOptions || {}),
-    })
+    });
   }
 
   try {
-    cached.client = await cached.promise
+    cached.client = await cached.promise;
   } catch (e: unknown) {
-    cached.promise = null
-    throw e
+    cached.promise = null;
+    throw e;
   }
 
-  return cached.client
-}
+  return cached.client;
+};
