71804e6104b6881324b9622c5800d6a4d67e64f3
Mohamed Abirou <abiroumohamed58@gmail.com>
2023-12-31 22:04:03 +0100

    fix: Images do nott show up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,43 +1,42 @@
-import { type ClassValue, clsx } from 'clsx'
-import { Metadata } from 'next'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT'
-    notation?: Intl.NumberFormatOptions['notation']
+    currency?: "USD" | "EUR" | "GBP" | "BDT";
+    notation?: Intl.NumberFormatOptions["notation"];
   } = {}
 ) {
-  const { currency = 'USD', notation = 'compact' } = options
+  const { currency = "USD", notation = "compact" } = options;
 
-  const numericPrice =
-    typeof price === 'string' ? parseFloat(price) : price
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
 
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
     currency,
     notation,
     maximumFractionDigits: 2,
-  }).format(numericPrice)
+  }).format(numericPrice);
 }
 
 export function constructMetadata({
-  title = 'Premiumark - the marketplace for digital assets',
-  description = 'Premiumark is an open-source marketplace for high-quality digital goods.',
-  image = '/thumbnail.jpg',
-  icons = '/favicon.ico',
+  title = "Premiumark - the marketplace for digital assets",
+  description = "Premiumark is an open-source marketplace for high-quality digital goods.",
+  image = "/thumbnail.jpg",
+  icons = "/favicon.ico",
   noIndex = false,
 }: {
-  title?: string
-  description?: string
-  image?: string
-  icons?: string
-  noIndex?: boolean
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
 } = {}): Metadata {
   return {
     title,
@@ -52,19 +51,19 @@ export function constructMetadata({
       ],
     },
     twitter: {
-      card: 'summary_large_image',
+      card: "summary_large_image",
       title,
       description,
       images: [image],
-      creator: '@MohamedAbirou',
+      creator: "@MohamedAbirou",
     },
     icons,
-    metadataBase: new URL('https://premiumark-production.up.railway.app/'),
+    metadataBase: new URL("http://localhost:3000/"),
     ...(noIndex && {
       robots: {
         index: false,
         follow: false,
       },
     }),
-  }
+  };
 }
